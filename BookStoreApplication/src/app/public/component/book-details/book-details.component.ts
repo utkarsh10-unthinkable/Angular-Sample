17cbe64df3f1d2f17514441255f49510c5d7f06f
utkarsh goyal <utkarsh.goyal@unthinkable.co>
2023-01-12 17:39:08 +0530

    first commit

--- BookStoreApplication/src/app/public/component/book-details/book-details.component.ts
+++ BookStoreApplication/src/app/public/component/book-details/book-details.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { __param } from 'tslib';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.css']
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit {
+  public Id:number=0;
+  constructor(private route:ActivatedRoute){}
+  ngOnInit(): void {
+    console.log(this.route)
+    this.route.params.subscribe((param)=>{
+      this.Id=param['id']
+     
+    })
+  }
+
 
 }
